f48c051722a1a99f379b0dbba54c5a3008c84444
なるみ <weaper@gmail.com>
2018-08-01 01:07:56 +0800

    remove jovyan and modify sh files

--- env/start-notebook.sh
+++ env/start-notebook.sh
@@ -4,13 +4,4 @@
 
 set -e
 
-if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
-  # launched by JupyterHub, use single-user entrypoint
-  exec /usr/local/bin/start-singleuser.sh $*
-else
-  if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
-    . /usr/local/bin/start.sh jupyter lab $*
-  else
-    . /usr/local/bin/start.sh jupyter notebook $*
-  fi
-fi
+. /usr/local/bin/start.sh jupyter notebook $*
